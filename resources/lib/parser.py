4e2c5a2e755715c96f06de5c78f02eec77bef51e
anis3 <anis3.n@web.de>
2018-11-23 18:00:55 +0000

    [parser] add re.search

--- resources/lib/parser.py
+++ resources/lib/parser.py
@@ -1,10 +1,10 @@
+# -*- coding: utf-8 -*-
 import re
-
 
 class cParser:
     @staticmethod
-    def parseSingleResult(sHtmlContent, sPattern):
-        aMatches = re.compile(sPattern).findall(sHtmlContent)
+    def parseSingleResult(sHtmlContent, pattern):
+        aMatches = re.compile(pattern).findall(sHtmlContent)
         if len(aMatches) == 1:
             aMatches[0] = cParser.__replaceSpecialCharacters(aMatches[0])
             return True, aMatches[0]
@@ -15,18 +15,23 @@ class cParser:
         return sString.replace('\\/', '/')
 
     @staticmethod
-    def parse(sHtmlContent, sPattern, iMinFoundValue=1, ignoreCase=False):
+    def parse(sHtmlContent, pattern, iMinFoundValue=1, ignoreCase=False):
+        sHtmlContent = cParser.__replaceSpecialCharacters(sHtmlContent)
         if ignoreCase:
-            aMatches = re.compile(sPattern, re.DOTALL | re.I).findall(sHtmlContent)
+            aMatches = re.compile(pattern, re.DOTALL | re.I).findall(sHtmlContent)
         else:
-            aMatches = re.compile(sPattern, re.DOTALL).findall(sHtmlContent)
+            aMatches = re.compile(pattern, re.DOTALL).findall(sHtmlContent)
         if len(aMatches) >= iMinFoundValue:
             return True, aMatches
         return False, aMatches
 
     @staticmethod
-    def replace(sPattern, sReplaceString, sValue):
-        return re.sub(sPattern, sReplaceString, sValue)
+    def replace(pattern, sReplaceString, sValue):
+        return re.sub(pattern, sReplaceString, sValue)
+
+    @staticmethod
+    def search(sSearch, sValue):
+        return re.search(sSearch, sValue, re.IGNORECASE)
 
     @staticmethod
     def escape(sValue):
@@ -34,8 +39,8 @@ class cParser:
 
     @staticmethod
     def getNumberFromString(sValue):
-        sPattern = "\d+"
-        aMatches = re.findall(sPattern, sValue)
-        if (len(aMatches) > 0):
+        pattern = "\d+"
+        aMatches = re.findall(pattern, sValue)
+        if len(aMatches) > 0:
             return int(aMatches[0])
         return 0
