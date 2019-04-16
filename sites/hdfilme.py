4d53eaf4a2b2582ec031c526c7f4bccc5c5927b9
anis3 <anis3.n@web.de>
2019-04-16 17:43:28 +0000

    [hdfilme] fix

--- sites/hdfilme.py
+++ sites/hdfilme.py
@@ -90,12 +90,14 @@ def showEntries(entryUrl=False, sGui=False, sSearchText=False):
     if not entryUrl: entryUrl = params.getValue('sUrl')
     iPage = int(params.getValue('page'))
     oRequest = cRequestHandler(entryUrl + '&page=' + str(iPage) if iPage > 0 else entryUrl, ignoreErrors=(sGui is not False))
+    oRequest.addHeaderEntry('Referer', URL_MAIN)
+    oRequest.addHeaderEntry('Upgrade-Insecure-Requests', '1')
     sHtmlContent = oRequest.request()
     pattern = '<ul[^>]class="products row">(.*?)</ul>'
     isMatch, sContainer = cParser.parseSingleResult(sHtmlContent, pattern)
 
     if isMatch:
-        pattern = '<div class="box-product clearfix" data-popover.*?href="([^"]+).*?data-src="([^"]+).*?alt="([^"]+)'
+        pattern = '<div class="box-product clearfix" data-popover.*?href="([^"]+).*?data-src="([^"]+).*?title=".*?">([^<]+)'
         isMatch, aResult = cParser.parse(sContainer, pattern)
 
     if not isMatch:
@@ -105,9 +107,10 @@ def showEntries(entryUrl=False, sGui=False, sSearchText=False):
     cf = cRequestHandler.createUrl(entryUrl, oRequest)
     total = len(aResult)
     for sUrl, sThumbnail, sName in aResult:
+        sName = sName.replace(' stream', '')
         if sSearchText and not cParser().search(sSearchText, sName):
             continue
-        sThumbnail = sThumbnail.replace('_thumb', '').replace('https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&amp;refresh=31536000&amp;resize_w=135&amp;resize_h=185&amp;url=', '') + cf
+        sThumbnail = sThumbnail.replace('_thumb', '') + cf
         isMatch, sYear = cParser.parse(sName, "(.*?)\((\d*)\)")
         for name, year in sYear:
             sName = name
@@ -147,15 +150,15 @@ def showEpisodes():
     sUrl = urlEncode(params.getValue('entryUrl'),':|/')
     sThumbnail = params.getValue('sThumbnail')
     sHtmlContent = cRequestHandler(sUrl).request()
-    pattern = 'episode="([\d]+).*?-([\d]+)-stream.*?">([\d]+)'
+    pattern = 'class="new.*?-([\d]+)-stream.*?data-episode-id="([\d]+).*?([\d]+)'
     isMatch, aResult = cParser.parse(sHtmlContent, pattern)
 
     if not isMatch:
-        if not sGui: oGui.showInfo('xStream', 'Es wurde kein Eintrag gefunden')
+        oGui.showInfo('xStream', 'Es wurde kein Eintrag gefunden')
         return
 
     total = len(aResult)
-    for eID, sID, eNr in aResult:
+    for sID, eID, eNr in aResult:
         oGuiElement = cGuiElement('Folge ' + eNr , SITE_IDENTIFIER, "showHosterserie")
         oGuiElement.setThumbnail(sThumbnail)
         oGuiElement.setFanart(sThumbnail)
@@ -165,12 +168,13 @@ def showEpisodes():
     oGui.setView('episodes')
     oGui.setEndOfDirectory()
 
+
 def showHosterserie():
     eID = ParameterHandler().getValue('eID')
     sID = ParameterHandler().getValue('sID')
     rUrl = ParameterHandler().getValue('entryUrl')
     hosters = []
-    oRequest = cRequestHandler(URL_MAIN + '/movie/load-stream/' + sID + '/' + eID +  '?server=1')
+    oRequest = cRequestHandler(URL_MAIN + '/movie/load-stream/' + sID + '/' + eID +  '?')
     oRequest.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
     oRequest.addHeaderEntry('Referer', rUrl)
     sHtmlContent = oRequest.request()
@@ -190,6 +194,7 @@ def showHosterserie():
     if hosters:
         hosters.append('getHosterUrl')
     return hosters
+
 
 def showHosters():
     sUrl = urlEncode(ParameterHandler().getValue('entryUrl'),':|/')
@@ -238,7 +243,8 @@ def showSearch():
 
 def _search(oGui, sSearchText):
     showEntries(URL_SEARCH % sSearchText, oGui, sSearchText)
-    
+
+
 def urlEncode(sUrl, safe=''):
     import urllib
     return urllib.quote(sUrl, safe)
